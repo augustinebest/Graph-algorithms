057b28c21fd0b3a9309fb6866631e97c73f6ec2c
augustinebest <augustusbest00@gmaile.com>
2022-01-09 12:20:40 +0100

    added undirected path algorithm

---
+++ graph/undirected-path.js
@@ -0,0 +1,41 @@
+const undirectedPath = (edges, src, dst) => {
+  const graph = buildGraph(edges);
+  // Depth First
+  return hasPath(graph, src, dst, new Set());
+};
+
+const hasPath = (graph, src, dst, visited) => {
+  if (src === dst) return true;
+  if (visited.has(src)) return false;
+  visited.add(src);
+
+  for (const neighbor of graph[src]) {
+    if (hasPath(graph, neighbor, dst, visited) === true) {
+      return true;
+    }
+  }
+
+  return false;
+};
+
+const buildGraph = (edges) => {
+  const graph = {};
+  for (const edge of edges) {
+    const [a, b] = edge;
+    if (!(a in graph)) graph[a] = [];
+    if (!(b in graph)) graph[b] = [];
+    graph[a].push(b);
+    graph[b].push(a);
+  }
+  return graph;
+};
+
+const edges = [
+  ["i", "j"],
+  ["k", "i"],
+  ["m", "k"],
+  ["k", "l"],
+  ["o", "n"],
+];
+
+console.log(undirectedPath(edges, "i", "o"));
